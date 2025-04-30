a10adfac2e94c839320ca80f86d92803e9f5a6d0
Chiku Okechukwu <chikunwenyi@gmail.com>
2025-04-30 18:58:14 -0400

    adding my shi

---
+++ portfolio-chiku/portfolio/src/mainpage.ts
@@ -0,0 +1,15 @@
+import './mainpage.css';
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <h1 class="Title"> 
+    Chiku's Domain!
+    </h1>
+      <img class="Chiku" src="/assets/884AF75A-5E7B-4E96-AB00-29FBD4966FDF.png" alt="Portfolio" />
+    
+
+    <div>
+    
+    </div>
+  </div>
+`;
